ed15f7c88afaef17928e02ff018537b4e32832a6
Zach Rausnitz <zjraus@gmail.com>
2018-10-13 13:07:38 -0400

    Revert "Update"

--- Sources/FluentBenchmark/Benchmarks/BenchmarkTimestampable.swift
+++ Sources/FluentBenchmark/Benchmarks/BenchmarkTimestampable.swift
@@ -57,24 +57,14 @@ extension Benchmarker where Database: QuerySupporting {
         // Ensure that there is a substantial difference between `originalUpdatedAt` and `updatedAt`.
         Thread.sleep(forTimeInterval: 0.05)
         _ = try test(tanner.save(on: conn))
-        let secondUpdatedAt = tanner.updatedAt!
-        if secondUpdatedAt <= originalUpdatedAt {
+        if tanner.updatedAt! <= originalUpdatedAt {
             self.fail("new updated at should be greater")
         }
-        
-        // Ensure that there is a substantial difference between `secondUpdatedAt` and `thirdUpdatedAt`.
-        Thread.sleep(forTimeInterval: 0.05)
-        _ = try test(User<Database>.query(on: conn).filter(\.name == "Tanner").update(\.age, to: 24).run())
 
         let f = try test(User<Database>.query(on: conn).filter(\.name == "Tanner").first())
-        guard let fetched = f, let id = fetched.id, id == tanner.id else {
+        guard let fetched = f else {
             self.fail("could not fetch user")
             return
-        }
-        
-        let thirdUpdatedAt = fetched.updatedAt!
-        if thirdUpdatedAt <= secondUpdatedAt {
-            self.fail("third updated at should be even greater")
         }
 
         if !fetched.createdAt!.isWithin(seconds: 2, of: tanner.createdAt!) ||
